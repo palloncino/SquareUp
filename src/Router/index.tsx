dbdccb4444326f262b4cb5c54c9ebdac3189ad48
Antonio Guiotto <antonio@Antonios-MacBook-Pro.local>
2023-10-24 12:21:27 +0200

    revert

--- src/Router/index.tsx
+++ src/Router/index.tsx
@@ -1,19 +1,17 @@
-import React, {Suspense} from 'react';
+import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
-const Explorer = React.lazy(() => import('../features/Explorer'));
-const ChatUp = React.lazy(() => import('../features/ChatUp'));
-const Cropper = React.lazy(() => import('../features/Cropper'));
+import Explorer from '../features/Explorer';
+import ChatUp from '../features/ChatUp';
+import Cropper from '../features/Cropper';
 
 const Stack = createStackNavigator();
 
 export const Router = () => {
   return (
-    <Suspense fallback={null}>
-      <Stack.Navigator initialRouteName="Explorer">
-        <Stack.Screen name="Explorer" component={Explorer} />
-        <Stack.Screen name="ChatUp" component={ChatUp} />
-        <Stack.Screen name="Cropper" component={Cropper} />
-      </Stack.Navigator>
-    </Suspense>
+    <Stack.Navigator initialRouteName="Explorer">
+      <Stack.Screen name="Explorer" component={Explorer} />
+      <Stack.Screen name="ChatUp" component={ChatUp} />
+      <Stack.Screen name="Cropper" component={Cropper} />
+    </Stack.Navigator>
   );
 };
