237531bcc01bfaa907a85fd2a5b37044b5423f24
Antonio Guiotto <antonio@Antonios-MacBook-Pro.local>
2023-10-26 15:59:31 +0200

    renamed

--- ios/SquareUpTests/SquareUpTests.m
+++ ios/SquareUpTests/SquareUpTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface nativeapp04Tests : XCTestCase
+@interface SquareUpTests : XCTestCase
 
 @end
 
-@implementation nativeapp04Tests
+@implementation SquareUpTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
